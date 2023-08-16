d8fe9d2cf5a705df7f3be39216f72d7530da8445
Houman Mohammadi <houmane.el@gmail.com>
2023-08-16 22:32:03 +0200

    first commit

---
+++ src/main/java/iav/gridPath/Grid.java
@@ -0,0 +1,33 @@
+package iav.gridPath;
+
+public class Grid {
+
+    public static int countPathsLoop(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        // Initialize the first row and first column to 1
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill in the rest of the grid using a loop
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    public static int countPathRecursive(){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPathsLoop(3,3));
+    }
+}
